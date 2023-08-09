ef289803bed878bc93ac5f4ae4523a798481f8b9
zuzannawozniak <zuzanna.wk@interia.pl>
2023-08-09 20:11:57 +0200

    Initial commit

---
+++ src/main/java/com/electricalvehicles/ElectricalVehiclesApplication.java
@@ -0,0 +1,13 @@
+package com.electricalvehicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricalVehiclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricalVehiclesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/electricalvehicles/ElectricalVehiclesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electricalvehicles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricalVehiclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
