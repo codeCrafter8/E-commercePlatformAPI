cbeb2ca1ca71560ee420ad3f6caa9a36db5e2317
zuzannawozniak <zuzanna.wk@interia.pl>
2023-11-02 13:58:18 +0100

    Add scheduler package

---
+++ src/main/java/com/pricecomparison/scheduler/FetchScheduler.java
@@ -0,0 +1,17 @@
+package com.pricecomparison.scheduler;
+
+import com.pricecomparison.fetcher.FetcherManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class FetchScheduler {
+    private final FetcherManager fetcherManager;
+    @Scheduled(fixedRate = 10000)
+    //@Scheduled(cron = "0 0 * * * *")
+    public void fetchOffers() {
+        fetcherManager.fetchAll();
+    }
+}
---
+++ src/main/java/com/pricecomparison/scheduler/PriceEntriesScheduler.java
@@ -0,0 +1,16 @@
+package com.pricecomparison.scheduler;
+
+import com.pricecomparison.service.PriceEntryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PriceEntriesScheduler {
+    private final PriceEntryService priceEntryService;
+    @Scheduled(cron = "0 2 0 * * *")
+    public void createPriceEntries() {
+        priceEntryService.createPriceEntries();
+    }
+}
